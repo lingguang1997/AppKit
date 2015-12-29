e4a6ba0525b6f45fbff9586b770e1373459ac920
Zijiao Liu <lingguang1997@gmail.com>
2015-12-29 13:14:22 +0800

    Add NSString drawing category

---
+++ AppKit/Utilities/NSString+AKDrawing.h
@@ -0,0 +1,18 @@
+//
+//  NSString+AKDrawing.h
+//  AppKit
+//
+//  Created by Zijiao Liu on 12/29/15.
+//  Copyright © 2015 Zijiao Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NSString (AKDrawing)
+
+- (CGRect)ak_boundingRectWithFont:(UIFont *)font size:(CGSize)size;
+- (CGFloat)ak_HeightWithFont:(UIFont *)font fixedWidth:(CGFloat)fixedWidth;
+- (CGFloat)ak_WidthWithFont:(UIFont *)font fixedHeight:(CGFloat)fixedHeight;
+
+@end
---
+++ AppKit/Utilities/NSString+AKDrawing.m
@@ -0,0 +1,28 @@
+//
+//  NSString+AKDrawing.m
+//  AppKit
+//
+//  Created by Zijiao Liu on 12/29/15.
+//  Copyright © 2015 Zijiao Liu. All rights reserved.
+//
+
+#import "NSString+AKDrawing.h"
+
+@implementation NSString (AKDrawing)
+
+- (CGRect)ak_boundingRectWithFont:(UIFont *)font size:(CGSize)size {
+    NSStringDrawingOptions options = NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading;
+    NSDictionary *attributes = @{ NSFontAttributeName : font };
+    CGRect rect = [self boundingRectWithSize:size options:options attributes:attributes context:nil];
+    return rect;
+}
+
+- (CGFloat)ak_HeightWithFont:(UIFont *)font fixedWidth:(CGFloat)fixedWidth {
+    return CGRectGetHeight([self ak_boundingRectWithFont:font size:CGSizeMake(fixedWidth, MAXFLOAT)]);
+}
+
+- (CGFloat)ak_WidthWithFont:(UIFont *)font fixedHeight:(CGFloat)fixedHeight {
+    return CGRectGetHeight([self ak_boundingRectWithFont:font size:CGSizeMake(MAXFLOAT, fixedHeight)]);
+}
+
+@end
