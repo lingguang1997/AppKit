340bd7af0e02e647243817f4c6b962e09108f4df
Zijiao Liu <lingguang1997@gmail.com>
2016-01-17 21:31:32 -0800

    Access dataViewController items in section controller

--- AppKit/DataViewController/AKTableViewSectionController.m
+++ AppKit/DataViewController/AKTableViewSectionController.m
@@ -18,8 +18,6 @@
 # pragma mark - Data structurs for data
 @property (nonatomic) AKTableViewCellAdapterCache *cache;
 
-@property (nonatomic) NSArray *items;
-
 @end
 
 @implementation AKTableViewSectionController
@@ -40,24 +38,24 @@
 # pragma mark - AKTableViewConfiguration
 
 - (CGFloat)dataViewController:(AKDataViewController *)dataViewController item:(id<NSObject>)item heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    AKTableViewCellAdapter *adapter = [self _adapterAtRow:indexPath.row];
+    AKTableViewCellAdapter *adapter = [self _dataViewController:dataViewController adapterAtRow:indexPath.row];
     return [adapter dataViewController:dataViewController item:item heightForRowAtIndexPath:indexPath];
 }
 
 - (AKTableViewCell *)dataViewController:(AKDataViewController *)dataViewController item:(id<NSObject>)item cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    AKTableViewCellAdapter *adapter = [self _adapterAtRow:indexPath.row];
+    AKTableViewCellAdapter *adapter = [self _dataViewController:dataViewController adapterAtRow:indexPath.row];
     return [adapter dataViewController:dataViewController item:item cellForRowAtIndexPath:indexPath];
 }
 
 - (void)dataViewController:(AKDataViewController *)dataViewController item:(id<NSObject>)item didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    AKTableViewCellAdapter *adapter = [self _adapterAtRow:indexPath.row];
+    AKTableViewCellAdapter *adapter = [self _dataViewController:dataViewController adapterAtRow:indexPath.row];
     [adapter dataViewController:dataViewController item:item didSelectRowAtIndexPath:indexPath];
 }
 
 - (AKTableViewCellGroupStyle)dataViewController:(AKDataViewController *)dataViewController item:(id<NSObject>)item groupStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
     if (_grouped) {
-        BOOL isFirstItem = [self _isFirstItem:item];
-        BOOL isLastItem = [self _isLastItem:item];
+        BOOL isFirstItem = [self _dataViewController:dataViewController isFirstItem:item];
+        BOOL isLastItem = [self _dataViewController:dataViewController isLastItem:item];
         if (isFirstItem) {
             if (isLastItem) {
                 return AKTableViewCellGroupStyleTopAndBottom;
@@ -81,26 +79,29 @@
 
 # pragma mark - Private methods
 
-- (id)_itemAtRow:(NSInteger)row {
-    assert(_items.count > row);
+- (id)_dataViewController:(AKDataViewController *)dataViewController itemAtRow:(NSInteger)row {
+    NSArray *items = [dataViewController stream].items;
+    assert(items.count > row);
     assert(row >= 0);
-    return _items[row];
+    return items[row];
 }
 
-- (AKTableViewCellAdapter *)_adapterAtRow:(NSInteger)row {
-    id item = [self _itemAtRow:row];
+- (AKTableViewCellAdapter *)_dataViewController:(AKDataViewController *)dataViewController adapterAtRow:(NSInteger)row {
+    id item = [self _dataViewController:dataViewController adapterAtRow:row];
     return [_cache adapterForItemClass:[item class]];
 }
 
-- (BOOL)_isFirstItem:(id)item {
-    assert(_items.count);
-    return [_items[0] isEqual:item];
+- (BOOL)_dataViewController:(AKDataViewController *)dataViewController isFirstItem:(id)item {
+    NSArray *items = [dataViewController stream].items;
+    assert(items.count);
+    return [items[0] isEqual:item];
 }
 
-- (BOOL)_isLastItem:(id)item {
-    NSInteger lastItemIndex = _items.count - 1;
+- (BOOL)_dataViewController:(AKDataViewController *)dataViewController isLastItem:(id)item {
+    NSArray *items = [dataViewController stream].items;
+    NSInteger lastItemIndex = items.count - 1;
     assert(lastItemIndex);
-    return [_items[lastItemIndex] isEqual:item];
+    return [items[lastItemIndex] isEqual:item];
 }
 
 @end
