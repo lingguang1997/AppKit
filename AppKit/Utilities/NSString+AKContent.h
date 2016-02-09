e4fb1f48076ca5e2e1d8cd571ed74d25e8d205cb
Zijiao Liu <lingguang1997@gmail.com>
2016-02-08 22:35:13 -0800

    Add NSString number checking

---
+++ AppKit/Utilities/NSString+AKContent.h
@@ -0,0 +1,15 @@
+//
+//  NSString+AKContent.h
+//  AppKit
+//
+//  Created by Zijiao Liu on 2/8/16.
+//  Copyright © 2016 Zijiao Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (AKContent)
+
+- (BOOL)isNumber;
+
+@end
---
+++ AppKit/Utilities/NSString+AKContent.m
@@ -0,0 +1,17 @@
+//
+//  NSString+AKContent.m
+//  AppKit
+//
+//  Created by Zijiao Liu on 2/8/16.
+//  Copyright © 2016 Zijiao Liu. All rights reserved.
+//
+
+#import "NSString+AKContent.h"
+
+@implementation NSString (AKContent)
+
+- (BOOL)isNumber {
+    return [self rangeOfCharacterFromSet:[[NSCharacterSet decimalDigitCharacterSet] invertedSet]].location == NSNotFound;
+}
+
+@end
